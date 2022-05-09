7c54d666782964ca3593e280fbb8e1c335323a65
Felipe Veiga <felipe.veiga.trabalho@gmail.com>
2022-05-09 13:34:36 -0300

    Estado and Cidade Implementation

--- src/main/java/com/felipeveiga/RestApiExampleApplication.java
+++ src/main/java/com/felipeveiga/RestApiExampleApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.felipeveiga.domain.Categoria;
+import com.felipeveiga.domain.Cidade;
+import com.felipeveiga.domain.Estado;
 import com.felipeveiga.domain.Produto;
 import com.felipeveiga.repositories.CategoriaRepository;
+import com.felipeveiga.repositories.CidadeRepository;
+import com.felipeveiga.repositories.EstadoRepository;
 import com.felipeveiga.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,30 +23,47 @@ public class RestApiExampleApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiExampleApplication.class, args);
 	}
 	
-
 	@Override
 	public void run(String... args) throws Exception {
-
+		
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-
+		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
-
+		
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-
+		
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-
+		
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
 	}
 } 
