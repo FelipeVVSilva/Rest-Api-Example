35d9eb1369eb16de07ab74485666f0aba726a030
Felipe Veiga <felipe.veiga.trabalho@gmail.com>
2022-05-16 16:32:29 -0300

    Validação de email repetido

--- src/main/java/com/felipeveiga/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/felipeveiga/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.felipeveiga.domain.Cliente;
 import com.felipeveiga.domain.dto.ClienteNewDTO;
 import com.felipeveiga.domain.enums.TipoCliente;
+import com.felipeveiga.repositories.ClienteRepository;
 import com.felipeveiga.resources.exceptions.FieldMessage;
 import com.felipeveiga.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -31,6 +38,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj: " ,"CNPJ inválido"));
 		}
 		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email: " ,"Email já existe"));
+		}
 		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
