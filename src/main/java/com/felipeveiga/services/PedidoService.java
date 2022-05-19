72f254f7b9884968e7484b838ac922c97bc0174d
Felipe Veiga <felipe.veiga.trabalho@gmail.com>
2022-05-19 16:34:23 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/felipeveiga/services/PedidoService.java
+++ src/main/java/com/felipeveiga/services/PedidoService.java
@@ -6,15 +6,21 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.felipeveiga.domain.Cliente;
 import com.felipeveiga.domain.ItemPedido;
 import com.felipeveiga.domain.PagamentoComBoleto;
 import com.felipeveiga.domain.Pedido;
 import com.felipeveiga.domain.enums.EstadoPagamento;
+import com.felipeveiga.domain.security.UserSS;
 import com.felipeveiga.repositories.ItemPedidoRepository;
 import com.felipeveiga.repositories.PagamentoRepository;
 import com.felipeveiga.repositories.PedidoRepository;
+import com.felipeveiga.services.exceptions.AuthorizationException;
 import com.felipeveiga.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,6 +36,8 @@ public class PedidoService {
 	private ProdutoService produtoService;
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	@Autowired
+	private ClienteService clienteService;
 	
 	public Pedido findById(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
@@ -41,6 +49,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.findById(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		
@@ -58,5 +67,15 @@ public class PedidoService {
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticaeted();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cli = clienteService.findById(user.getId());
+		return repo.findByCliente(cli, pageRequest);	
 	}
 }
